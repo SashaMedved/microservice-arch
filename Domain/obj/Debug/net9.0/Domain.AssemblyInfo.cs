68da5823f9e91b9803f8655de4a3ae274223586e
SashaMedved <laskov98rus@list.ru>
2025-10-01 18:34:58 +0500

    Onion

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42020f35e544a516de19494dcb3c1a3904b49ec3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8485ee3344b3b5919857eabb237d88170735ee8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
